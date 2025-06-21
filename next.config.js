97d26e7648e9f46b1228294c6dda249d35f61ea0
Nephyy <142471741+Nephyy1@users.noreply.github.com>
2025-06-21 20:07:12 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -14,12 +14,6 @@ const nextConfig = {
         port: '',
         pathname: '/**',
       },
-      {
-        protocol: 'https',
-        hostname: 'jkt48.com',
-        port: '',
-        pathname: '/images/**',
-      },
     ],
   },
 };
