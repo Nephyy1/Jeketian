345f48dbeab76b5284d6caa9375f5af8824fd0e0
Nephyy <142471741+Nephyy1@users.noreply.github.com>
2025-06-23 14:59:59 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -14,6 +14,12 @@ const nextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'image.showroom-live.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
